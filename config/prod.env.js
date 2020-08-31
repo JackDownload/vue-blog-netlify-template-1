e0e54c491e91e741c1608cef2122ca85316a537a
JackDownload <kismocsy@gmail.com>
2020-08-31 18:28:35 +0200

    Update prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  COSMIC_BUCKET: `"${process.env.COSMIC_BUCKET || '87de4da0-eb9c-11ea-ae81-453b8452a5ab'}"`,
-  COSMIC_READ_KEY: `"${process.env.COSMIC_READ_KEY || '3xIYwRc8LN0AlJn0FnJrD5rStjg8OoIL7A402kXpyPVnId1zlc'}"`,
-  COSMIC_WRITE_KEY: `"${process.env.COSMIC_WRITE_KEY || 'zvSzTKtIcDhfhJVgKieLvhSMkEQ9562E8sp1PHuTbCbKIZUIhR'}"`,
+  COSMIC_BUCKET: `"${process.env.COSMIC_BUCKET || 'simple-vue-blog'}"`,
+  COSMIC_READ_KEY: `"${process.env.COSMIC_READ_KEY}"`,
+  COSMIC_WRITE_KEY: `"${process.env.COSMIC_WRITE_KEY}"`,
   BLOG_TITLE: `"${process.env.BLOG_TITLE || 'Vue Blog Cosmic JS'}"`,
-}
